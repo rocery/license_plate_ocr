9fcdd196e5b15c651fd145a5b331581c913dd265
rocery <sastranuralamsyah@gmail.com>
2025-01-29 22:15:42 +0700

    fix logging and the max size allowed to upload

--- start_ocr.sh
+++ start_ocr.sh
@@ -2,6 +2,10 @@
 
 # Navigate to the ocr directory
 cd
+if [ ! -d "license_plate_ocr" ]; then
+    echo "Directory license_plate_ocr does not exist."
+    exit 1
+fi
 cd license_plate_ocr || exit
 
 # Activate the virtual environment
@@ -9,9 +13,12 @@ source ./venv/bin/activate
 
 # Check if port 5000 is in use and kill the process if it is
 if fuser 5000/tcp > /dev/null 2>&1; then
-    echo "Port 5001 is in use. Killing the process..."
+    echo "Port 5000 is in use. Killing the process..."
     fuser -k 5000/tcp
 fi
 
 # Run the Python application in the background and redirect output to a log file
-python3 app.py > license_plate_ocr.log 2>&1 &
+nohup python3 -u app.py > license_plate_ocr.log 2>&1 &
+
+# Optional: Sleep for a few seconds to allow the app to start
+sleep 2
